86866a42da36a58e74b350ad2554b5de4c80f00e
ChopinGuo <guoshaopan@pku.edu.cn>
2017-10-19 17:36:39 +0800

    add new functions
    + splitDataSet
    + chooseBestFeatureToSplit

--- TREES/trees.py
+++ TREES/trees.py
@@ -30,3 +30,50 @@ def calcShannonEnt(dataSet):
         shannonEnt -= prob * log(prob, 2)  # 以2为底求对数
 
     return shannonEnt
+
+
+# 按照给定的特征划分数据集
+
+def splitDataSet(dataSet, axis, value):
+
+    # 创建新的list对象
+    retDataSet = []
+
+    # 抽取
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+
+    return retDataSet
+
+
+# 选择最好的数据集划分方式
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+
+    for i in range(numFeatures):
+        
+        # 创建唯一的分类标签列表
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        
+        newEntropy = 0.0
+
+        # 计算每种划分方式的信息熵
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+
+        # 计算最好的信息增益
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+
+    return bestFeature
